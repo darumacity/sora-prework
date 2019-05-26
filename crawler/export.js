2bf13b6f3e0ae9bd7f49d24804bef71ea41ca2b1
darumacity <lt012071@gmail.com>
2019-05-27 03:30:52 +0900

    create data directory if not exists

--- crawler/export.js
+++ crawler/export.js
@@ -2,6 +2,11 @@
 
 module.exports.writeFile = (value) => {
     var fs = require('fs');
+
+    if (!fs.existsSync('../data')) {
+        fs.mkdirSync('../data');
+    }
+
     fs.writeFile('../data/201907.json', JSON.stringify(value), err => {
         if (err) {
             console.log(err);
